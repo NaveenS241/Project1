d9018a3141cbee618264232960f0359fd084154b
NaveenS241 <naveenhosur24@gmail.com>
2025-02-24 11:54:15 +0530

    first commit

--- demo/src/App.js
+++ demo/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Cricket – The Gentleman’s Game</h1>
+        <h1>Cricket World</h1>
         <article>
           <h2>Introduction to Cricket</h2>
           <p>
